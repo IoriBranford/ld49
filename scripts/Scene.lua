677b7d4b615bc9c60ad299a124efb2d62b27b35b
IoriBranford <ioribranford@gmail.com>
2021-10-01 18:21:33 -0700

    Allow choosing which map layer types to load.

--- scripts/Scene.lua
+++ scripts/Scene.lua
@@ -377,22 +377,24 @@ function Scene:addObject(object)
     return self:addShapeObject(object)
 end
 
-function Scene:addMap(map)
+function Scene:addMap(map, layerfilter)
     local function addLayers(layers)
         for i = 1, #layers do
             local layer = layers[i]
             local layertype = layer.type
             if layer.type == "group" then
                 addLayers(layer)
-            elseif layertype == "tilelayer" then
-                layer.sprites = self:addTileLayer(layer)
-            elseif layertype == "objectgroup" then
-                for i = 1, #layer do
-                    local object = layer[i]
-                    object.sprite = self:addObject(object)
+            elseif not layerfilter or layerfilter:find(layertype) then
+                if layertype == "tilelayer" then
+                    layer.sprites = self:addTileLayer(layer)
+                elseif layertype == "objectgroup" then
+                    for i = 1, #layer do
+                        local object = layer[i]
+                        object.sprite = self:addObject(object)
+                    end
+                elseif layertype == "imagelayer" then
+                    layer.sprite = self:addImageLayer(layer)
                 end
-            elseif layertype == "imagelayer" then
-                layer.sprite = self:addImageLayer(layer)
             end
         end
     end
