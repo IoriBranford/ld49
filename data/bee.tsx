b5479bbceea78c1dcf70ead4879d74f7d2de955c
IoriBranford <ioribranford@gmail.com>
2021-10-03 17:14:39 -0700

    New bee sprite.

---
+++ data/bee.tsx
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="bee" tilewidth="64" tileheight="64" tilecount="2" columns="2" objectalignment="center">
+ <image source="bee.png" width="128" height="64"/>
+ <tile id="0" type="Bee">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="16" y="16" width="32" height="32">
+    <properties>
+     <property name="collidable" type="bool" value="true"/>
+    </properties>
+    <ellipse/>
+   </object>
+  </objectgroup>
+  <animation>
+   <frame tileid="0" duration="50"/>
+   <frame tileid="1" duration="50"/>
+  </animation>
+ </tile>
+ <tile id="1" type="Bee">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="16" y="16" width="32" height="32">
+    <properties>
+     <property name="collidable" type="bool" value="true"/>
+    </properties>
+    <ellipse/>
+   </object>
+  </objectgroup>
+ </tile>
+</tileset>
